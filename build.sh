7f5cd4db819ff2f3326c449275b607b5e7745a7d
TSTman <tstman@protonmail.com>
2019-07-07 16:27:20 -0500

    Add script to build icons

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail;
+cd "$(dirname "$0")";
+
+webextension_dir='webextension';
+
+for size in 16 48 96 128;
+do
+    printf 'Building icon of size %s...\n' "$size";
+    convert icon.svg -resize "${size}x${size}" "${webextension_dir}/icons/${size}.png";
+done;
