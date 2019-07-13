1b1bf221b60c92347971acb6dd5f07394865e297
TSTman <tstman@protonmail.com>
2019-07-13 01:54:02 -0500

    Put icons directory inside assets directory

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ webextension_dir='webextension';
 for size in 16 48 96 128;
 do
     printf 'Building icon of size %s...\n' "$size";
-    convert icon.svg -resize "${size}x${size}" "${webextension_dir}/icons/${size}.png";
+    convert icon.svg -resize "${size}x${size}" "${webextension_dir}/assets/icons/${size}.png";
 done;
