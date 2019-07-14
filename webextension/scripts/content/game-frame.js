9a929bca45da57ceaecc1dd3a5f1a53f022c351e
TSTman <tstman@protonmail.com>
2019-07-14 14:50:42 -0500

    No neeed to load game in an iframe

--- webextension/scripts/content/game-frame.js
+++ webextension/scripts/content/game-frame.js
@@ -21,7 +21,13 @@ function replaceBody(game) {
         document.documentElement
     );
 
-    let cbids = {
+    let baseUrls = {
+            'default': '/play-tetris-content/resources/project-tetriscom/game/',
+            'sanrio': '/games-content/sanrio01/resources/project-tetriscom-sanrio01/game/',
+            'mb': '/play-tetris-content-mb/resources/project-tetriscom-MB/game/',
+        },
+        baseUrl = baseUrls[game],
+        cbids = {
             'default': '1222606EA8579670',
             'sanrio': 'E378AF83A71EF6C8',
             'mb': '7F01C3BC86595C76',
@@ -46,6 +52,13 @@ function replaceBody(game) {
                 height: 100vh;
             }
         `;
+
+    document
+        .head
+        .appendChild(
+            document.createElement('base')
+        )
+        .setAttribute('href', baseUrl);
 
     document
         .body
